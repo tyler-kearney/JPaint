fcabec8f226d8cd8ba5ac0a22e4a497d53e477ed
Tyler Kearney <75549435+darkwolf9817@users.noreply.github.com>
2024-07-24 22:59:05 -0500

    Built the main form.

---
+++ src/JPaint.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class JPaint {
+    private JComboBox cbxBrushSize;
+    private JTextField txtColorChange;
+    private JButton btnColorChange;
+    private JButton btnEraser;
+    private JButton btnSave;
+    private JButton btnLoad;
+    private JPanel drawingCanvas;
+}
