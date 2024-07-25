3def0aa78677fe68bc22d4f2d003613d61c48b59
Tyler Kearney <75549435+darkwolf9817@users.noreply.github.com>
2024-07-25 02:00:27 -0500

    Set up the Main.java file.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JPaint panel = new JPaint();
+        JFrame frame = new JFrame("JPaint");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
