72405101054489d9cda004ef0f3d3f7f377527ab
Tyler Kearney <75549435+darkwolf9817@users.noreply.github.com>
2024-07-25 22:25:58 -0500

    The application does function. Need to fix the eraser method.

--- src/JPaint.java
+++ src/JPaint.java
@@ -6,6 +6,8 @@ import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Optional;
 
 
 public class JPaint extends JFrame {
@@ -15,14 +17,26 @@ public class JPaint extends JFrame {
     private JButton btnEraser;
     private JButton btnSave;
     private JButton btnLoad;
-    private JPanel drawingCanvas;
+    private JPanel mainPanel;
 
-    protected int previousX, previousY;
+    protected int previousX = -1;
+    protected int previousY = -1;
     public Color currentColor = Color.BLACK;
+    protected ArrayList<Point> points = new ArrayList<>();
+    private DrawingCanvas drawingCanvas;
 
     public JPaint() {
-        cbxBrushSize = new JComboBox<>(new Integer[]{5, 10, 15, 20, 25});
-
+        getContentPane().add(mainPanel);
+        cbxBrushSize.addItem(5);
+        cbxBrushSize.addItem(10);
+        cbxBrushSize.addItem(15);
+        cbxBrushSize.addItem(20);
+        cbxBrushSize.addItem(25);
+        System.out.println(mainPanel);
+        drawingCanvas = new DrawingCanvas();
+        drawingCanvas.setVisible(true);
+        mainPanel.add(drawingCanvas, BorderLayout.CENTER);
+        System.out.println(drawingCanvas);
         btnColorChange.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -37,7 +51,7 @@ public class JPaint extends JFrame {
         btnEraser.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
+                currentColor = Color.WHITE;
             }
         });
         btnSave.addMouseListener(new MouseAdapter() {
@@ -65,29 +79,53 @@ public class JPaint extends JFrame {
 
                 try {
                     loadedImage = ImageIO.read(new File("/"));
-                    repaint();
+                    drawingCanvas.repaint();
                 } catch(IOException ex) {
                     ex.printStackTrace();
                 }
             }
         });
+
+        drawingCanvas.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                points.add(e.getPoint());
+                previousX = e.getX();
+                previousY = e.getY();
+                drawingCanvas.repaint();
+            }
+        });
+
         drawingCanvas.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseDragged(MouseEvent e) {
-                int x = e.getX();
-                int y = e.getY();
-                int brushSize = (int) cbxBrushSize.getSelectedItem();
-
-                draw(x, y, currentColor, brushSize);
+                points.add(e.getPoint());
+                previousX = e.getX();
+                previousY = e.getY();
+                drawingCanvas.repaint();
             }
         });
     }
-    private void draw(int x, int y, Color selectedColor, int brushSize) {
-        Graphics2D g2d = (Graphics2D) getGraphics();
-        g2d.setColor(selectedColor);
-        g2d.setStroke(new BasicStroke(brushSize));
-        g2d.drawLine(previousX, previousY, x, y);
-        previousX = x;
-        previousY = y;
+
+    class DrawingCanvas extends JPanel {
+        public DrawingCanvas() {
+            super();
+            setMinimumSize(new Dimension(600, 400));
+        }
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2d = (Graphics2D) g;
+            g2d.setColor(currentColor);
+
+            int brushSize = cbxBrushSize.getItemAt(cbxBrushSize.getSelectedIndex());
+            g2d.setStroke(new BasicStroke(brushSize));
+
+            for (int i = 1; i < points.size(); i++) {
+                Point p1 = points.get(i - 1);
+                Point p2 = points.get(i);
+                g2d.drawLine(p1.x, p1.y, p2.x, p2.y);
+            }
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,15 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         JPaint panel = new JPaint();
+
+        /*
         JFrame frame = new JFrame("JPaint");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
         frame.add(panel);
-        frame.pack();
-        frame.setVisible(true);
+
+         */
+        panel.pack();
+        panel.setVisible(true);
     }
 }
